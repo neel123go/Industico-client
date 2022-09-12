f73c98fdeac52dda4d34310935f941a8dcafea06
neel123go <ayounpaul1029@gmail.com>
2022-09-12 19:40:20 +0600

    commit before deploy

--- src/Firebase.init.js
+++ src/Firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyC-cldEDK91qTKvTdEBmFNvJGJwk6W0GmE",
-    authDomain: "industico.firebaseapp.com",
-    projectId: "industico",
-    storageBucket: "industico.appspot.com",
-    messagingSenderId: "160246909770",
-    appId: "1:160246909770:web:f50dc8f7f1915cfb450fb3"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
